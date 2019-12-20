6b6887f037b76b4fd827f34693b245f86af66f96
Symbolk <symbolk@163.com>
2019-12-20 19:50:32 +0800

    [chore] Reorganize the way to config inputs.

--- src/main/java/com/github/smartcommit/client/Config.java
+++ src/main/java/com/github/smartcommit/client/Config.java
@@ -1,12 +1,15 @@
 package com.github.smartcommit.client;
 
+import java.io.File;
+
 /** Store the constants as the config */
 public final class Config {
   public static final String REPO_NAME = "nomulus";
-  public static final String REPO_PATH = "/Users/symbolk/coding/data/" + REPO_NAME;
-  public static final String TEMP_DIR = "/Users/symbolk/coding/data/temp";
-
   public static final String COMMIT_ID = "906b054f4b7a2e38681fd03282996955406afd65";
+  public static final String DATA_DIR = "/Users/symbolk/coding/data";
+
+  public static final String REPO_PATH = DATA_DIR + File.separator + REPO_NAME;
+  public static final String TEMP_DIR = DATA_DIR + File.separator + "temp";
   public static final String JRE_PATH =
-      "/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home/jre/lib/rt.jar";
+      System.getProperty("java.home") + File.separator + "lib/rt.jar";
 }
