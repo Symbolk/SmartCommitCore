b8d423e86b61b5d0d42a77fa8bc7c1cc1a76f27a
chuncen <lichuncen@outlook.com>
2020-02-07 00:10:47 +0800

    [feature] proceed repos given in specific pathway and cloned from git

--- src/main/java/com/github/smartcommit/intent/MultiCommitInfoHandler.java
+++ src/main/java/com/github/smartcommit/intent/MultiCommitInfoHandler.java
@@ -1,6 +1,8 @@
 package com.github.smartcommit.intent;
 
 import com.github.smartcommit.intent.CommitInfoHandler;
+import com.github.smartcommit.util.Utils;
+
 import java.io.*;
 import java.io.IOException;
 
@@ -16,16 +18,45 @@ public class MultiCommitInfoHandler {
         // Line by line
         String line = bufferedReader.readLine();
         String collectionName = line;
+        line = bufferedReader.readLine();
+        String tempDir = line;
+
         while (line != null){
             try {
                 line = bufferedReader.readLine();
-                System.out.println(line);
-                String[] args0 = new String[]{line, collectionName};
+                // get RepoName and tempLocation
+
+                File folder = new File(line);
+                String[] args0;
+                // pwd
+                if (folder.exists()) { // found it in given local pathway
+                    args0 = new String[]{line, collectionName};
+                }
+                // url
+                else {
+                    // the length of ".git" is 4
+                    int index = line.lastIndexOf(File.separator);
+                    String repoName = line.substring(index+1, line.length()-4);
+                    String tempLocation = tempDir+File.separator+repoName;
+                    folder = new File(tempLocation);
+
+                    if(folder.exists()) {           // found it in local temp dir
+                        args0 = new String[]{tempLocation, collectionName};
+                    }
+                    else {                            // clone from git
+                    String log = Utils.runSystemCommand(tempDir, "git", "clone", line);
+                    System.out.print(log);
+                    args0 = new String[]{tempLocation, collectionName};
+                    }
+                }
+                System.out.println("working in " + args0[0]);
                 CommitInfoHandler.main(args0);
+
             } catch (Exception e) {
-                e.printStackTrace();
+                //e.printStackTrace();
             }
         }
+
         bufferedReader.close();
         fileReader.close();
     }
