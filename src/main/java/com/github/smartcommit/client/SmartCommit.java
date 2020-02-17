6d96305b4fe3761e92905d7dc2e25530dd9b538a
Symbolk <symbolk@163.com>
2020-02-17 20:34:22 +0800

    [feature] Add functions to generate patches from manual group files.

--- src/main/java/com/github/smartcommit/client/SmartCommit.java
+++ src/main/java/com/github/smartcommit/client/SmartCommit.java
@@ -14,12 +14,15 @@ import com.github.smartcommit.model.graph.Node;
 import com.github.smartcommit.util.Utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jgrapht.Graph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -49,7 +52,6 @@ public class SmartCommit {
     this.repoPath = repoPath;
     this.tempDir = tempDir;
     this.id2DiffHunkMap = new HashMap<>();
-    Utils.clearDir(tempDir);
   }
 
   public void setDetectRefactorings(Boolean detectRefactorings) {
@@ -71,6 +73,8 @@ public class SmartCommit {
    * @throws Exception
    */
   public Map<String, Group> analyzeWorkingTree() throws Exception {
+    Utils.clearDir(tempDir);
+
     // 1. analyze the repo
     RepoAnalyzer repoAnalyzer = new RepoAnalyzer(repoID, repoName, repoPath);
     List<DiffFile> diffFiles = repoAnalyzer.analyzeWorkingTree();
@@ -107,69 +111,26 @@ public class SmartCommit {
    * @throws Exception
    */
   public Map<String, Group> analyzeCommit(String commitID) throws Exception {
-    {
-      // 1. analyze the repo
-      RepoAnalyzer repoAnalyzer = new RepoAnalyzer(repoID, repoName, repoPath);
-      List<DiffFile> diffFiles = repoAnalyzer.analyzeCommit(commitID);
-      List<DiffHunk> allDiffHunks = repoAnalyzer.getDiffHunks();
+    Utils.clearDir(tempDir);
 
-      if (diffFiles.isEmpty() || allDiffHunks.isEmpty()) {
-        logger.info("Files are unchanged at commit: " + commitID);
-        return null;
-      }
+    // 1. analyze the repo
+    RepoAnalyzer repoAnalyzer = new RepoAnalyzer(repoID, repoName, repoPath);
+    List<DiffFile> diffFiles = repoAnalyzer.analyzeCommit(commitID);
+    List<DiffHunk> allDiffHunks = repoAnalyzer.getDiffHunks();
 
-      this.id2DiffHunkMap = repoAnalyzer.getIdToDiffHunkMap();
-
-      // 2. collect the data into temp dir
-      DataCollector dataCollector = new DataCollector(repoName, tempDir);
-      Pair<String, String> dataPaths = dataCollector.collectDiffFilesAtCommit(commitID, diffFiles);
-
-      Map<String, Group> results = analyze(diffFiles, allDiffHunks, dataPaths);
-      return results;
-    }
-  }
-
-  /**
-   * Generate and save the details of the grouping results
-   *
-   * @param results
-   */
-  public void generateGroupDetails(Map<String, Group> results) {
-    Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-    List<String> groupedDiffHunks = new ArrayList<>();
-    for (Map.Entry<String, Group> entry : results.entrySet()) {
-      String path =
-          tempDir + File.separator + "details" + File.separator + entry.getKey() + ".json";
-      StringBuilder builder = new StringBuilder();
-      builder.append(entry.getValue().getIntentLabel()).append("\n");
-      for (String id : entry.getValue().getDiffHunks()) {
-        if (groupedDiffHunks.contains(id)) {
-          DiffHunk diffHunk = id2DiffHunkMap.get(id.split(":")[1]);
-          logger.error("Duplicate DiffHunk: " + diffHunk.getUniqueIndex());
-        }
-        groupedDiffHunks.add(id);
-        String[] pair = id.split(":");
-        if (pair.length == 2) {
-          builder.append("------------").append("\n");
-          DiffHunk diffHunk = id2DiffHunkMap.get(pair[1]);
-          builder.append(diffHunk.getUniqueIndex()).append("\n");
-          builder.append(diffHunk.getDescription()).append("\n");
-          builder.append(gson.toJson(diffHunk.getBaseHunk())).append("\n");
-          builder.append(gson.toJson(diffHunk.getCurrentHunk())).append("\n");
-        } else {
-          logger.error("Invalid id: " + id);
-        }
-      }
-      Utils.writeStringToFile(builder.toString(), path);
+    if (diffFiles.isEmpty() || allDiffHunks.isEmpty()) {
+      logger.info("Files are unchanged at commit: " + commitID);
+      return null;
     }
 
-    if (groupedDiffHunks.size() != id2DiffHunkMap.keySet().size()) {
-      logger.error(
-          "Incorrect #diffhunks: Actual/Expected= "
-              + groupedDiffHunks.size()
-              + "/"
-              + id2DiffHunkMap.keySet().size());
-    }
+    this.id2DiffHunkMap = repoAnalyzer.getIdToDiffHunkMap();
+
+    // 2. collect the data into temp dir
+    DataCollector dataCollector = new DataCollector(repoName, tempDir);
+    Pair<String, String> dataPaths = dataCollector.collectDiffFilesAtCommit(commitID, diffFiles);
+
+    Map<String, Group> results = analyze(diffFiles, allDiffHunks, dataPaths);
+    return results;
   }
 
   /**
@@ -219,5 +180,111 @@ public class SmartCommit {
         DiffGraphExporter.exportAsDotWithType(groupGenerator.getDiffHunkGraph());
 
     return groupGenerator.getGeneratedGroups();
+  }
+
+  /**
+   * Generate and save the details of the grouping results
+   *
+   * @param results
+   */
+  public void generateGroupDetails(Map<String, Group> results) {
+    Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+    List<String> groupedDiffHunks = new ArrayList<>();
+    for (Map.Entry<String, Group> entry : results.entrySet()) {
+      String path =
+          tempDir + File.separator + "details" + File.separator + entry.getKey() + ".json";
+      StringBuilder builder = new StringBuilder();
+      builder.append(entry.getValue().getIntentLabel()).append("\n");
+      for (String id : entry.getValue().getDiffHunks()) {
+        if (groupedDiffHunks.contains(id)) {
+          DiffHunk diffHunk = id2DiffHunkMap.get(id.split(":")[1]);
+          logger.error("Duplicate DiffHunk: " + diffHunk.getUniqueIndex());
+        }
+        groupedDiffHunks.add(id);
+        String[] pair = id.split(":");
+        if (pair.length == 2) {
+          builder.append("------------").append("\n");
+          DiffHunk diffHunk = id2DiffHunkMap.get(pair[1]);
+          builder.append(diffHunk.getUniqueIndex()).append("\n");
+          builder.append(diffHunk.getDescription()).append("\n");
+          builder.append(gson.toJson(diffHunk.getBaseHunk())).append("\n");
+          builder.append(gson.toJson(diffHunk.getCurrentHunk())).append("\n");
+        } else {
+          logger.error("Invalid id: " + id);
+        }
+      }
+      Utils.writeStringToFile(builder.toString(), path);
+    }
+
+    if (groupedDiffHunks.size() != id2DiffHunkMap.keySet().size()) {
+      logger.error(
+          "Incorrect #diffhunks: Actual/Expected= "
+              + groupedDiffHunks.size()
+              + "/"
+              + id2DiffHunkMap.keySet().size());
+    }
+  }
+
+  /**
+   * Generate patch files according to the manual group results
+   *
+   * @param tempDir
+   */
+  public void generatePatches(String tempDir) throws FileNotFoundException {
+    String manualGroupsDir = tempDir + File.separator + "manual_groups";
+    String fileDiffsDir = tempDir + File.separator + "diffs";
+    String patchesDir = tempDir + File.separator + "patches";
+    Utils.clearDir(patchesDir);
+
+    List<String> groupFilePaths = Utils.listAllJsonFilePaths(manualGroupsDir);
+    Gson gson = new Gson();
+    for (String path : groupFilePaths) {
+      StringBuilder builder = new StringBuilder();
+      // read and parse group json file
+      JsonReader reader = new JsonReader(new FileReader(path));
+      Group group = gson.fromJson(reader, Group.class);
+      // put diff hunks within the same file together
+      Map<String, List<String>> fileID2hunkIDsMap = new HashMap<>();
+      for (String id : group.getDiffHunks()) {
+        Pair<String, String> idPair = Utils.parseUUIDs(id);
+        if (idPair != null) {
+          String fileID = idPair.getLeft();
+          String diffHunkID = idPair.getRight();
+          if (fileID2hunkIDsMap.containsKey(fileID)) {
+            fileID2hunkIDsMap.get(fileID).add(diffHunkID);
+          } else {
+            List<String> temp = new ArrayList<>();
+            temp.add(diffHunkID);
+            fileID2hunkIDsMap.put(fileID, temp);
+          }
+        } else {
+          logger.error("Null idPair with " + id);
+        }
+      }
+
+      // read and parse the diff json by file id
+      for (Map.Entry<String, List<String>> entry : fileID2hunkIDsMap.entrySet()) {
+        String fileDiffPath = fileDiffsDir + File.separator + entry.getKey() + ".json";
+        reader = new JsonReader(new FileReader(fileDiffPath));
+        DiffFile diffFile = gson.fromJson(reader, DiffFile.class);
+        // get headers and raw diffs
+        builder
+            .append(String.join(System.lineSeparator(), diffFile.getRawHeaders()))
+            .append(System.lineSeparator());
+        for (String diffHunkID : entry.getValue()) {
+          DiffHunk diffHunk = diffFile.getDiffHunksMap().getOrDefault(diffHunkID, null);
+          if (diffHunk != null) {
+            builder
+                .append(String.join(System.lineSeparator(), diffHunk.getRawDiffs()))
+                .append(System.lineSeparator());
+          } else {
+            logger.error("Null diffHunk with id: " + diffHunkID);
+          }
+        }
+      }
+      // save patches in temp dir
+      String resultPath = patchesDir + File.separator + group.getGroupID() + ".patch";
+      Utils.writeStringToFile(builder.toString(), resultPath);
+    }
   }
 }
