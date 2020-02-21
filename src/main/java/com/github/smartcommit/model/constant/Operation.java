8027aeffa07e496cad90f430ade03badf01be718
Symbolk <symbolk@163.com>
2020-02-21 17:33:37 +0800

    [feature] List example types in Operation.

--- src/main/java/com/github/smartcommit/model/constant/Operation.java
+++ src/main/java/com/github/smartcommit/model/constant/Operation.java
@@ -1,8 +1,24 @@
 package com.github.smartcommit.model.constant;
 
-/**
- * Action operation on AST or Refactoring
- */
+/** Action operation on AST or Refactoring */
 public enum Operation {
+  // AST operations
+  ADD("Add"),
+  DEL("Delete"),
+  UPD("Update"),
+  MOV("Move"),
 
+  // Refactoring operations
+  EXTRACT("Extract");
+
+  public String label;
+
+  Operation(String label) {
+    this.label = label;
+  }
+
+  @Override
+  public String toString() {
+    return label;
+  }
 }
