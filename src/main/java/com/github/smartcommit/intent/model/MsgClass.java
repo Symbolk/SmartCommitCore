443eaf70948c079cfec013b78a092243198f224c
chuncen <lichuncen@outlook.com>
2020-02-29 09:57:10 +0800

    [doc] add enum MsgClass as concluded by haisheng

--- src/main/java/com/github/smartcommit/intent/model/MsgClass.java
+++ src/main/java/com/github/smartcommit/intent/model/MsgClass.java
@@ -4,5 +4,33 @@ package com.github.smartcommit.intent.model;
  * Classification of commit message
  */
 public enum MsgClass {
+    ADD("Add"),
+    CRT("Create"),
+    IMP("Implement"),
+    MAK("Make"),
+    UPD("Update"),
+    USE("Use"),
+    SET("Set"),
+    HDL("Handle"),
+    IMV("Improve"),
+    OPT("Optimize"),
+    UPG("Upgrade"),
+    RMV("Remove"),
+    RFC("Refactor"),
+    REP("Replace"),
+    MOV("Move"),
+    CHG("Change"),
+    RNM("Rename"),
+    DOC("Document"),
+    RFM("Reformat"),
+    FIX("Fix"),
+    RVT("Revert"),
+    TST("Test");
+
+    public String label;
+
+    MsgClass(String label) {
+        this.label = label;
+    }
 
 }
