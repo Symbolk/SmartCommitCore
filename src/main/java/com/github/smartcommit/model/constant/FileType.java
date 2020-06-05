8a8eb43b4f8c921f90bcea0481b5c561ef026a4e
Symbolk <symbolk@163.com>
2020-06-05 23:23:45 +0800

    [feature] Add more types of file and content.

--- src/main/java/com/github/smartcommit/model/constant/FileType.java
+++ src/main/java/com/github/smartcommit/model/constant/FileType.java
@@ -1,15 +1,23 @@
 package com.github.smartcommit.model.constant;
 
-/**
- * The type of the diff File
- */
+/** The type of the diff File */
 public enum FileType {
   JAVA(".java", "Java"),
+  JSON(".json", "Json"),
+  JS(".javascript", "JavaScript"),
+  PY(".py", "Python"),
+  CPP(".cpp", "C++"),
+  HPP(".hpp", "C++ Header"),
+  C(".c", "C"),
+  H(".h", "C Header"),
   MD(".md", "Markdown"),
   TXT(".txt", "Text"),
-  JAR(".jar", "Jar"),
   XML(".xml", "XML"),
-  OTHER(".*", "Other");
+  GRADLE(".gradle", "Groovy"),
+  PROP(".properties", "Properties"),
+
+  BIN(".", "Binary"), // binary file
+  OTHER(".*", "Other"); // other plain text file
 
   public String extension;
   public String label;
